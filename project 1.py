64095a6a6172fb7fb8d98b3ead4c6d8ff48b603c
TriggeredAdhu <168425539+TriggeredAdhu@users.noreply.github.com>
2024-04-29 16:11:04 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,7 @@
+#Project 1
+# Python program to convert Fahrenheit to Celsius
+
+F= int(input("enter the temperature"))  # F= Fahrenheit
+C=(F-32)*5/9                             # C= celsius
+
+print("temperature in celsius",C)
