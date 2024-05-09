86434540cc0c628a6d37e95e053de9dd9471eff2
TriggeredAdhu <168425539+TriggeredAdhu@users.noreply.github.com>
2024-05-09 11:24:46 +0530

    Add files via upload

---
+++ project 3.py
@@ -0,0 +1,20 @@
+# Project 3
+# Get 5 student names and for each student get 5 subjects marks. Display the student name and total marks.
+
+for i in range(5):
+    name = (input("enter the student name:"))
+    total = 0
+    for i in range(5):
+        marks = int(input("enter the mark of the student:"))
+        total += marks
+    print("the total mark of", name, "is", total)
+
+
+
+
+
+
+
+
+
+
